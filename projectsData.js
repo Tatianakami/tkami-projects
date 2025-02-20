f8bc1a840287ff44d0dce61d0f91668e74c80d63
eduardolentz <eduardo.lentz@gmail.com>
2025-02-20 18:40:32 -0300

    Adicionando projetos

--- projectsData.js
+++ projectsData.js
@@ -1,10 +1,20 @@
 // projectsData.js
 const projectsData = {
     Cloud: [
-      { title: "Projeto Cloud 1", tags: ["AWS"], description: "Site estático no S3", site: "", repo: "", article:"" },
+      { title: "Site estático na S3", 
+        tags: ["AWS"], 
+        description: "Hospedando site estático no S3 da AWS", 
+        site: "", 
+        repo: "", 
+        article:"https://medium.com/@eduardolentz/como-hospedei-minha-landing-page-profissional-na-aws-s3-um-guia-passo-a-passo-fc33b9ebc454" },
     ],
     Python: [
-      { title: "Projeto Python 1", tags: ["Data Science"], description: "Descrição breve 1.", site: "", repo: "", article:"" },
+      { title: "Exercícios Python ", 
+        tags: ["Data Science"], 
+        description: "Exercícios e desafios do curso do André Iacono", 
+        site: "", 
+        repo: "https://github.com/eduardolentz/python-udemy-andreiacono", 
+        article:"" },
     ],
     DevOps: [
       { title: "Projeto DevOps 1", tags: ["Docker"], description: "Em construção", site: "", repo: "", article:"" },
